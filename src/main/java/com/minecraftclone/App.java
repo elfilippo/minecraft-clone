53583b88b5583720955009e4fdb3daca64d991c1
Mats Oeynhausen <m.oeynhausen@icloud.com>
2026-01-21 08:32:59 +0100

    fixed

--- src/main/java/com/minecraftclone/App.java
+++ src/main/java/com/minecraftclone/App.java
@@ -10,15 +10,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         var cam = new GLKamera();
-        var FPSCamera = new FPSCamera(cam);
         var texture = new GLTextur("C:\\Users\\MatsOeynhausen\\OneDrive - Ruhrtal-Gymnasium Schwerte\\Dokumente\\Schule\\Schnellablage\\minecraft-clone\\src\\main\\resources\\grass.jpg");
         new GLLicht();
         new GLBoden(texture);
         new GLHimmel(texture);
         var keys = new GLTastatur();
-        try {
-            new CameraDebug();
-        } catch (Exception e) {}
         while (true) {
             try {
                 Thread.sleep(16); // ~60 FPS
