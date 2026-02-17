9a171cf47a878ab3e2fca12915a4054e787b658c
Mats Oeynhausen <m.oeynhausen@icloud.com>
2026-02-17 01:03:51 +0100

    clean up

--- src/main/java/com/minecraftclone/render/RenderEngine.java
+++ src/main/java/com/minecraftclone/render/RenderEngine.java
@@ -21,6 +21,9 @@ public class RenderEngine {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        gui.inventoryDisplayItem(1, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
+        gui.inventoryDisplayItem(4, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
     }
 
     public void guiUpdate() {
@@ -28,45 +31,5 @@ public class RenderEngine {
         gui.setHunger(player.getHunger());
         gui.changeHotbarSlot(player.getHotbarSlot());
         gui.setInventoryVisibility(player.getinventoryVisible());
-
-        gui.inventoryDisplayItem(1, 1, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 2, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 3, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 4, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 5, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 6, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 7, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 8, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(1, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
-
-        gui.inventoryDisplayItem(2, 1, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 2, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 3, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 4, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 5, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 6, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 7, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 8, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(2, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
-
-        gui.inventoryDisplayItem(3, 1, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 2, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 3, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 4, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 5, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 6, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 7, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 8, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(3, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
-
-        gui.inventoryDisplayItem(4, 1, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 2, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 3, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 4, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 5, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 6, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 7, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 8, new ItemInstance(ItemRegistry.get("iron_sword")));
-        gui.inventoryDisplayItem(4, 9, new ItemInstance(ItemRegistry.get("iron_sword")));
     }
 }
